4ff1c598dc63fd255d622eddc4c1ac1e095a6f26
mironkkkkk <163337961+mironkkkkk@users.noreply.github.com>
2025-04-16 21:49:37 +0300

    Add files via upload

---
+++ 16.04.25/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Num {
+        private int min;
+        private int max;
+
+        public Num(int a, int b) {
+            if (a > b) {
+                this.min = b;
+                this.max = a;
+            }
+            else {
+                this.min = a;
+                this.max = b;
+            }
+        }
+
+        public void even() {
+            for (int i = min; i < max; i++) {
+                if (i % 2 == 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public void odd() {
+            for (int i = min; i < max; i++) {
+                if (i % 2 == 1) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public void def() {
+            List<bool> arr = new List<bool>();
+            bool flag = false;
+            for (int i = 2; i < min; i++) {
+                if (min % i == 0) {
+                    arr.Add(false);
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                arr.Add(true);
+            }
+
+            flag = false;
+
+            for (int i = 2; i < max; i++) {
+                if (max % i == 0) {
+                    arr.Add(false);
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                arr.Add(true);
+            }
+
+            Console.WriteLine($"{min}: {arr[0]}, {max}: {arr[1]}");
+        }
+
+        public bool def(int a)
+        {
+            for (int i = 2; i < a; i++) {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool fib(int a) {
+            int zero = 0;
+            bool flag = false;
+            int one = 1;
+            for (int i = 0; i < a; i++) {
+                int b = one + zero;
+                zero = one;
+                one = b;
+                if (b == a) {
+                    flag = true;
+                }
+                if (b > a) {
+                    break;
+                }
+            }
+            return flag;
+        }
+
+    }
+    internal class Class1
+    {
+    }
+}
---
+++ 16.04.25/Class2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Figure {
+        public void square(int a) {
+            for (int i = 0; i < a; i++) {
+                for (int j = 0; j < a; j++) {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void rectangle(int a, int b) {
+            for (int i = 0; i < b; i++) {
+                for (int j = 0; j < a; j++) {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void troian(int a) {
+            int c = a + 1;
+            int count = 1;
+            for (int i = 0; i < a; i++) {
+                for (int j = 0; j < c / 2; j++) {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < count; k++) {
+                    Console.Write("* ");
+                }
+                count++;
+                c -= 2;
+                if (c == 0) {
+                    c = 2;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    internal class Class2
+    {
+    }
+}
