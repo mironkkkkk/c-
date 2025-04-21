c5ad1395fae17deb5361702c2690c5305ace88d6
mironkkkkk <163337961+mironkkkkk@users.noreply.github.com>
2025-04-21 21:15:32 +0300

    Add files via upload

---
+++ 21.04.25/Class2.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Matrix
+    {
+        private int row;
+        private int column;
+        private int[,] arr;
+
+        public Matrix(int row, int column) {
+            this.row = row;
+            this.column = column;
+            this.arr = new int[row, column];
+            for (int i = 0; i < this.row; i++) {
+                for (int j = 0; j < this.column; j++) {
+                    arr[i, j] = j;
+                }
+            }
+        }
+
+        public void print() {
+            for (int i = 0; i < this.row; i++) {
+                for (int j = 0; j < this.column; j++) {
+                    Console.Write($"{arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static Matrix operator +(Matrix arr1, Matrix arr2) {
+            Matrix arr3 = new Matrix(arr1.row, arr1.column);
+            for (int i = 0; i < arr1.row; i++) {
+                for (int j = 0; j < arr1.column; j++) {
+                    arr3.arr[i, j] = arr1.arr[i, j] + arr2.arr[i, j];
+                }
+            }
+            return arr3;
+        }
+
+        public static Matrix operator -(Matrix arr1, Matrix arr2)
+        {
+            Matrix arr3 = new Matrix(arr1.row, arr1.column);
+            for (int i = 0; i < arr1.row; i++)
+            {
+                for (int j = 0; j < arr1.column; j++)
+                {
+                    arr3.arr[i, j] = arr1.arr[i, j] - arr2.arr[i, j];
+                }
+            }
+            return arr3;
+        }
+
+        public static Matrix operator *(Matrix arr1, Matrix arr2)
+        {
+            Matrix arr3 = new Matrix(arr1.row, arr1.column);
+            for (int i = 0; i < arr1.row; i++)
+            {
+                for (int j = 0; j < arr1.column; j++)
+                {
+                    arr3.arr[i, j] = arr1.arr[i, j] * arr2.arr[i, j];
+                }
+            }
+            return arr3;
+        }
+
+        public static bool operator ==(Matrix arr1, Matrix arr2)
+        {
+            for (int i = 0; i < arr1.row; i++)
+            {
+                for (int j = 0; j < arr1.column; j++)
+                {
+                    if (arr1.arr[i, j] != arr2.arr[i, j]) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static bool operator !=(Matrix arr1, Matrix arr2)
+        {
+            for (int i = 0; i < arr1.row; i++)
+            {
+                for (int j = 0; j < arr1.column; j++)
+                {
+                    if (arr1.arr[i, j] != arr2.arr[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ 21.04.25/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee worker1 = new Employee("Dima", 14, 200);
+            //worker1.print();
+            //worker1 += 100;
+            //worker1.print();
+            //Console.WriteLine(worker1.Name);
+
+            Matrix arr1 = new Matrix(4,4);
+            Matrix arr2 = new Matrix(4, 4);
+            (arr1 + arr2).print();
+            (arr1 - arr2).print();
+            (arr1 * arr2).print();
+            Console.WriteLine(arr1 == arr2);
+        }
+    }
+}
