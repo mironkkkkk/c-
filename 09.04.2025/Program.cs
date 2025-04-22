e64ab9180f9187e4159f8cd65a29827d9b9d2883
mironkkkkk <163337961+mironkkkkk@users.noreply.github.com>
2025-04-22 19:22:03 +0300

    Add files via upload

---
+++ 09.04.2025/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // задание 1
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if (a > 100 || a < 0)
+            //{
+            //    Console.WriteLine("Ошибка");
+            //}
+            //else {
+            //    if (a % 3 == 0 && a % 5 == 0)
+            //    {
+            //        Console.WriteLine("Fizz Buzz");
+            //    }
+            //    else if (a % 3 == 0)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //    else if (a % 5 == 0)
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }
+            //    else {
+            //        Console.WriteLine(a);
+            //    }
+            //}
+
+
+            // задание 2
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(a * (b / 100));
+
+            // задание 3
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //int d = Convert.ToInt32(Console.ReadLine());
+            //int newNum = a * 1000 + b * 100 + c * 10 + d;
+            //Console.WriteLine(newNum);
+
+            // задание 4
+            //string a = Console.ReadLine();
+            //int id1 = Convert.ToInt32(Console.ReadLine());
+            //int id2 = Convert.ToInt32(Console.ReadLine());
+
+            //char[] arr = a.ToCharArray();
+            //char temp = arr[id1];
+            //arr[id1] = arr[id2];
+            //arr[id2] = temp;
+
+            //string c = "";
+            //foreach (char item in arr) {
+            //    c += item;
+            //}
+            //int fin = Convert.ToInt32(c);
+            //Console.WriteLine(fin);
+
+            // задание 5
+            //string a = Console.ReadLine();
+            //string[] arr = a.Split('.');
+
+            //if (Convert.ToInt32(arr[1]) == 1 || Convert.ToInt32(arr[1]) == 2 || Convert.ToInt32(arr[1]) == 12)
+            //{
+            //    Console.WriteLine("Winter");
+            //}
+            //else if (Convert.ToInt32(arr[1]) == 3 || Convert.ToInt32(arr[1]) == 4 || Convert.ToInt32(arr[1]) == 5)
+            //{
+            //    Console.WriteLine("Spring");
+            //}
+            //else if (Convert.ToInt32(arr[1]) == 6 || Convert.ToInt32(arr[1]) == 7 || Convert.ToInt32(arr[1]) == 8)
+            //{
+            //    Console.WriteLine("Summer");
+            //}
+            //else {
+            //    Console.WriteLine("Autumn");
+            //}
+
+            // задание 6
+            //double a = Convert.ToDouble(Console.Read());
+            //Console.WriteLine("1 - Цельсия в Фаренгейты 2 - Фаренгейтов в Цельсия");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //if (b == 1)
+            //{
+            //    Console.WriteLine((a - 32) * 5 / 9);
+            //}
+            //else {
+            //    Console.WriteLine((a - 32) * 5 / 9);
+            //}
+
+            // задание 7
+            //int min = Convert.ToInt32(Console.ReadLine());
+            //int max = Convert.ToInt32(Console.ReadLine());
+            //if (min > max)
+            //{
+            //    int t = min;
+            //    min = max;
+            //    max = t;
+            //}
+
+            //for (int i = min; i <= max; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.Write(i + " ");
+            //    }
+            //}
+        }
+    }
+}
