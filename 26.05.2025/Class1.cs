8ac0ce1f18ea4c78f02c8c2fc2f71d2ec438fd45
mironkkkkk <163337961+mironkkkkk@users.noreply.github.com>
2025-05-26 21:06:14 +0300

    Add files via upload

---
+++ 26.05.2025/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ClassList
+{
+    public class Class1
+    {
+
+    }
+
+    public class PC {
+        private string brand;
+        private int numSeria;
+        private int price;
+        private bool case_battle = true;
+        private bool onOrOff = false;
+
+        public string Brand {
+            set { this.brand = value; }
+            get { return brand; }
+        }
+
+        public int NumSeria {
+            set { this.numSeria = value; }
+            get { return numSeria; }
+        }
+
+        public int Price{
+            set { this.price = value; }
+            get { return price; }
+        }
+
+        public bool Case_battle {
+            set { this.case_battle = value; }
+            get { return case_battle; }
+        }
+
+        public bool OnOrOff {
+            set { this.onOrOff = value; }
+            get { return onOrOff; }
+        }
+
+        public PC() {
+            this.brand = "";
+            this.numSeria = 0;
+            this.price = 0;
+        }
+
+        public PC(string str, int num1, int num2, bool flag) {
+            this.brand = str;
+            this.numSeria = num1;
+            this.price = num2;
+            this.case_battle = flag;
+        }
+
+        public PC(string str, int num1, int num2) {
+            this.brand = str;
+            this.numSeria = num1;
+            this.price = num2;
+        }
+
+
+
+    }
+}
---
+++ 26.05.2025/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassList;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassList.PC a1 = new ClassList.PC();
+            ClassList.PC a2 = new ClassList.PC();
+            ClassList.PC a3 = new ClassList.PC();
+            ClassList.PC a4 = new ClassList.PC();
+            ClassList.PC a5 = new ClassList.PC();
+
+            List<PC> arr = new List<PC>();
+            arr.Add(a1);
+            arr.Add(a2);
+            arr.Add(a3);
+            arr.Add(a4);
+            arr.Add(a5);
+
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\Взрослая академия\\Desktop\\ClassList\\postol.csv", append: true)) {
+                foreach (var item in arr) {
+                    writer.WriteLineAsync($"brand: {item.Brand} num: {item.NumSeria} price: {item.Price} case_battle {item.Case_battle} onOrOff {item.OnOrOff}");
+                }
+                writer.WriteLineAsync("\n");
+            }
+        }
+    }
+}
