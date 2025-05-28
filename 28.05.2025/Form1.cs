a5a2fa961830787c2c2f9e7182771b7848811ecc
mironkkkkk <163337961+mironkkkkk@users.noreply.github.com>
2025-05-28 21:38:50 +0300

    Add files via upload

---
+++ 28.05.2025/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        List<string> arrCategory = new List<string> { "Продукты", "Транспорт", "Развлечения" };
+        List<int> arrCount = new List<int> { 0,0,0,};
+        bool flagShow = true;
+        string textBtnShow = "Скрыть";
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var item in arrCategory) {
+                comboBox1.Items.Add(item);
+            }
+            foreach (var item in arrCategory) {
+                comboBox2.Items.Add(item);
+            }
+
+            using (StreamReader reader = new StreamReader("C:\\Users\\Взрослая академия\\Desktop\\WindowsFormsApp3\\file.csv"))
+            {
+                string[] str = reader.ReadLine().Split(',');
+                arrCount[0] = Convert.ToInt32(str[0]);
+                arrCount[1] = Convert.ToInt32(str[1]);
+                arrCount[2] = Convert.ToInt32(str[2]);
+                labelProduct.Text = Convert.ToString(arrCount[0]);
+                labelTransport.Text = Convert.ToString(arrCount[1]);
+                labelGaiety.Text = Convert.ToString(arrCount[2]);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string choiceCategory = comboBox1.SelectedItem.ToString();
+            int userText = Convert.ToInt32(textBox1.Text);
+            if (Convert.ToString(userText) == "" || choiceCategory == "") {
+                MessageBox.Show($"Произошла ошибка, некоторые поля пустые");
+            }
+            else {
+                for (int i = 0; i < arrCategory.Count; i++) {
+                    if (arrCategory[i] == choiceCategory) {
+                        arrCount[i] += userText;
+                    }
+                }
+            }
+            labelProduct.Text =  Convert.ToString(arrCount[0]);
+            labelTransport.Text = Convert.ToString(arrCount[1]);
+            labelGaiety.Text = Convert.ToString(arrCount[2]);
+            writer();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            labelProduct.Visible = flagShow;
+            labelTransport.Visible = flagShow;
+            labelGaiety.Visible = flagShow;
+            flagShow = !flagShow;
+
+            button2.Text = textBtnShow;
+            if (textBtnShow == "Показать") { textBtnShow = "Скрыть"; }
+            else { textBtnShow = "Показать"; }
+            writer();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string choiceCategory = comboBox2.SelectedItem.ToString();
+            string userNewText = textBox2.Text;
+            if (userNewText == "" || choiceCategory == "") {
+                MessageBox.Show($"Произошла ошибка, некоторые поля пустые");
+            }
+            else {
+                for (int i = 0; i < arrCategory.Count; i++) {
+                    if (arrCategory[i] == choiceCategory) {
+                        arrCount[i] = Convert.ToInt32(userNewText);
+                    }
+                }
+            }
+            labelProduct.Text = Convert.ToString(arrCount[0]);
+            labelTransport.Text = Convert.ToString(arrCount[1]);
+            labelGaiety.Text = Convert.ToString(arrCount[2]);
+            writer();
+        }
+
+        public void writer() {
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\Взрослая академия\\Desktop\\WindowsFormsApp3\\file.csv", append: false))
+            {
+                writer.WriteLine($"{labelProduct.Text},{labelTransport.Text},{labelGaiety.Text}");
+            }
+        }
+    }
+}
---
+++ 28.05.2025/Form2.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(611, 262);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Статус выполнения";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(614, 278);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(479, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Задача";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(482, 278);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(482, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Добавить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
