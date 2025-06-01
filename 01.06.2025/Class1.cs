4db9fe8c300e6ab4d89495a9977799e28edde585
mironkkkkk <163337961+mironkkkkk@users.noreply.github.com>
2025-06-01 22:15:11 +0300

    Add files via upload

---
+++ 01.06.2025/Class1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Web
+    {
+        private string name;
+        private string url;
+        private string descr;
+        private string ip;
+
+        public Web(string name, string url, string descr, string ip) { 
+            this.name = name;
+            this.url = url; 
+            this.descr = descr;
+            this.ip = ip;
+        }
+
+        public string Name { 
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Url
+        {
+            get { return url; }
+            set { url = value; }
+        }
+
+        public string Descr
+        {
+            get { return descr; }
+            set { descr = value; }
+        }
+
+        public string Ip
+        {
+            get { return ip; }
+            set { ip = value; }
+        }
+
+        public void print() {
+            Console.WriteLine($"name: {name} url: {url} descr: {descr} ip: {ip}");
+        }
+    }
+
+    internal class Journal
+    {
+        private string name;
+        private string data;
+        private string descr;
+        private string number;
+        private string mail;
+
+        public Journal(string name, string data, string descr, string number, string mail)
+        {
+            this.name = name;
+            this.data = data;
+            this.descr = descr;
+            this.number = number;
+            this.mail = mail;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public string Descr
+        {
+            get { return descr; }
+            set { descr = value; }
+        }
+
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public string Mail
+        {
+            get { return mail; }
+            set { mail = value; }
+        }
+
+        public void print()
+        {
+            Console.WriteLine($"name: {name} data: {data} descr: {descr} number: {number} mail: {mail}");
+        }
+    }
+
+    internal class Magazin
+    {
+        private string name;
+        private string adress;
+        private string descr;
+        private string number;
+        private string mail;
+
+        public Magazin(string name, string adress, string descr, string number, string mail)
+        {
+            this.name = name;
+            this.adress = adress;
+            this.descr = descr;
+            this.number = number;
+            this.mail = mail;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Adress
+        {
+            get { return adress; }
+            set { adress = value; }
+        }
+
+        public string Descr
+        {
+            get { return descr; }
+            set { descr = value; }
+        }
+
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public string Mail
+        {
+            get { return mail; }
+            set { mail = value; }
+        }
+
+        public void print()
+        {
+            Console.WriteLine($"name: {name} adress: {adress} descr: {descr} number: {number} mail: {mail}");
+        }
+    }
+}
---
+++ 01.06.2025/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //square('*', 5);
+            //bool flag = pal(231332);
+            //Console.WriteLine(flag);
+            int[] q1 = { 1, 2, 6, 1, 88, 7, 6 };
+            int[] q2 = { 6,88,7 };
+            int[] arr = arrSort(q1,q2);
+            foreach (var item in arr) {
+                Console.Write(item + " ");
+            }
+        }
+
+        static public void square(char b, int a) {
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    Console.Write(b + " ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+        static public bool pal(int a) {
+            string s = Convert.ToString(a);
+            for (int i = 0, j = s.Length - 1; i < s.Length; i++, j--) {
+                if (s[i] != s[j]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static public int[] arrSort(int[] arrBase, int[] arrAdd)
+        {
+            List<int> list1 = new List<int>(arrBase);
+            List<int> list2 = new List<int>(arrAdd);
+
+            List<int> result = new List<int>();
+
+            foreach (var item in list1)
+            {
+                if (!list2.Contains(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+    }
+}
