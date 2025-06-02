13b96b47f240b15383ca76d34c73b5801b1c816f
mironkkkkk <163337961+mironkkkkk@users.noreply.github.com>
2025-06-02 21:12:51 +0300

    Add files via upload

---
+++ 02.06.2025/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxWord1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBoxDelete = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxAddRus = new System.Windows.Forms.TextBox();
+            this.textBoxAddEnglish = new System.Windows.Forms.TextBox();
+            this.textBoxAddUkr = new System.Windows.Forms.TextBox();
+            this.textBoxAddBel = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(248, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Word";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxWord1
+            // 
+            this.textBoxWord1.Location = new System.Drawing.Point(251, 264);
+            this.textBoxWord1.Name = "textBoxWord1";
+            this.textBoxWord1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWord1.TabIndex = 2;
+            this.textBoxWord1.TextChanged += new System.EventHandler(this.textBoxWord1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(460, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Result";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(460, 271);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 13);
+            this.labelResult.TabIndex = 3;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(251, 349);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(378, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Error";
+            this.label3.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(251, 304);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(463, 304);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // textBoxDelete
+            // 
+            this.textBoxDelete.Location = new System.Drawing.Point(42, 268);
+            this.textBoxDelete.Name = "textBoxDelete";
+            this.textBoxDelete.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDelete.TabIndex = 8;
+            this.textBoxDelete.TextChanged += new System.EventHandler(this.textBoxDelete_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Delete";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxAddRus
+            // 
+            this.textBoxAddRus.Location = new System.Drawing.Point(631, 118);
+            this.textBoxAddRus.Name = "textBoxAddRus";
+            this.textBoxAddRus.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddRus.TabIndex = 11;
+            // 
+            // textBoxAddEnglish
+            // 
+            this.textBoxAddEnglish.Location = new System.Drawing.Point(631, 162);
+            this.textBoxAddEnglish.Name = "textBoxAddEnglish";
+            this.textBoxAddEnglish.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddEnglish.TabIndex = 12;
+            // 
+            // textBoxAddUkr
+            // 
+            this.textBoxAddUkr.Location = new System.Drawing.Point(631, 247);
+            this.textBoxAddUkr.Name = "textBoxAddUkr";
+            this.textBoxAddUkr.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddUkr.TabIndex = 14;
+            // 
+            // textBoxAddBel
+            // 
+            this.textBoxAddBel.Location = new System.Drawing.Point(631, 203);
+            this.textBoxAddBel.Name = "textBoxAddBel";
+            this.textBoxAddBel.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddBel.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(628, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Add";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(631, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(575, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Rus";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(575, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Engl";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(575, 250);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Ukr";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(575, 210);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(22, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Bel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxAddUkr);
+            this.Controls.Add(this.textBoxAddBel);
+            this.Controls.Add(this.textBoxAddEnglish);
+            this.Controls.Add(this.textBoxAddRus);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxDelete);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.textBoxWord1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxWord1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBoxDelete;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxAddRus;
+        private System.Windows.Forms.TextBox textBoxAddEnglish;
+        private System.Windows.Forms.TextBox textBoxAddUkr;
+        private System.Windows.Forms.TextBox textBoxAddBel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ 02.06.2025/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        List<string> arr = new List<string>();
+        List<string> language = new List<string>() { "Русский", "Англиский", "Белоруский", "Украинский"};
+        public Form1()
+        {
+            InitializeComponent();
+            using (StreamReader reader = new StreamReader("C:\\Users\\Взрослая академия\\Desktop\\WindowsFormsApp4\\file.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string str = reader.ReadLine();
+                    arr.Add(str);
+                }
+            }
+
+            foreach (var item in language) {
+                comboBox1.Items.Add(item);
+                comboBox2.Items.Add(item);
+            }
+        }
+
+        private void textBoxWord1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            bool flag = false;
+            string box1 = comboBox1.SelectedItem.ToString();
+            string box2 = comboBox2.SelectedItem.ToString();
+
+            if (textBoxWord1.Text == "" || box1 == "" || box2 == "") {
+                label3.Visible = true;
+            }
+            else {
+                label3.Visible = false;
+                string wordUser = textBoxWord1.Text;
+                int languageId1 = 0;
+                int languageId2 = 0;
+                for (int i = 0; i < language.Count; i++) {
+                    if (box1 == language[i]) {
+                        languageId1 = i;
+                    }
+                    if (box2 == language[i]) {
+                        languageId2 = i;
+                    }
+                }
+                foreach (string item in arr) {
+                    if (flag) {
+                        label3.Visible = false;
+                        break; 
+                    }
+                    string[] wordArr = item.Split('.');
+                    if (wordArr[languageId1].ToLower() == wordUser.ToLower()) {
+                        labelResult.Text = wordArr[languageId2];
+                        flag = true;
+                    }
+                }
+                if (!flag) {
+                    label3.Visible = true;
+                }
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool flagDelete = false;
+            string wordDelete = textBoxDelete.Text;
+            if (wordDelete == "") {
+                label3.Visible = true;
+            }
+            else {
+                label3.Visible = false;
+                foreach (var item in arr) {
+                    string[] arrWord = item.Split('.');
+                    for (int i = 0; i < arrWord.Length; i++) {
+                        if (flagDelete) { break; } 
+                        if (arrWord[i] == wordDelete) {
+                            arr.Remove(item);
+                            flagDelete = true;
+                        }
+                    }
+                    if (flagDelete) { break; }
+                }
+                using (StreamWriter writer = new StreamWriter("C:\\Users\\Взрослая академия\\Desktop\\WindowsFormsApp4\\file.csv", append: false))
+                {
+                    foreach (var item in arr) {
+                        writer.WriteLine(item);
+                    }
+                }
+            }
+        }
+
+        private void textBoxDelete_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string wordRus = textBoxAddRus.Text;
+            string wordEngl = textBoxAddEnglish.Text;
+            string wordBel = textBoxAddBel.Text;
+            string wordUkr = textBoxAddUkr.Text;
+
+            if (wordRus == "" || wordEngl == "" || wordBel == "" || wordUkr == "") {
+                label3.Visible = true;
+            }
+            else {
+                label3.Visible = false;
+                string newWord = $"{wordRus}.{wordEngl}.{wordBel}.{wordUkr}";
+                arr.Add(newWord);
+                using (StreamWriter writer = new StreamWriter("C:\\Users\\Взрослая академия\\Desktop\\WindowsFormsApp4\\file.csv", append: false))
+                {
+                    foreach (var item in arr)
+                    {
+                        writer.WriteLine(item);
+                    }
+                }
+            }
+        }
+    }
+}
