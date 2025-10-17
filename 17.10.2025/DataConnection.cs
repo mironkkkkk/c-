c8df72b46efc1ba71641c4b1ee3afdff27e432be
mironkkkkk <163337961+mironkkkkk@users.noreply.github.com>
2025-10-17 21:24:41 +0300

    Add files via upload

---
+++ 17.10.2025/DataConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp7
+{
+    static class DataConnection
+    {
+
+        public static async Task<List<string>> showData() {
+            using (SqlConnection SqlConnect = new SqlConnection(@"Server=(localdb)\test;Database=master;Trusted_Connection=True")) {
+                List<string> values = new List<string>();
+                string sqlText = $"select * from orders";
+                await SqlConnect.OpenAsync();
+                SqlCommand command = new SqlCommand(sqlText, SqlConnect);
+                SqlDataReader reader = command.ExecuteReader();
+                using (reader) {
+                    while (reader.Read()) {
+                        int id = reader.IsDBNull(0) ? -1:reader.GetInt32(0);
+                        int price = reader.IsDBNull(0) ? -1 : reader.GetInt32(1);
+                        DateTime date = reader.IsDBNull(0) ? Convert.ToDateTime("0000.00.00") : reader.GetDateTime(2);
+                        string name = reader.IsDBNull(0) ? null : reader.GetString(3);
+                        values.Add(Convert.ToString(id));
+                        values.Add(Convert.ToString(price));
+                        values.Add(Convert.ToString(date));
+                        values.Add(name);
+                        return values;
+                    }
+                }
+                return values;
+
+            }
+        }
+
+
+
+    }
+}
---
+++ 17.10.2025/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Task arr = DataConnection.showData();
+            List<string> list = await arr;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
